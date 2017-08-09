74f0e7a4644171fb79a67738ca3351d31aa99b19
kuangwentao <wt.kuang@icloud.com>
2017-08-09 21:40:24 +1200

    Add real workload dataset and transfer format

---
+++ CBO/dataTransfer/DataTransfer.java
@@ -0,0 +1,51 @@
+package dataTransfer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class DataTransfer {
+	public static void main(String[] args) {
+		int workloadSize = 500;
+        String[][] workload=new String[1250][workloadSize];
+		for(int j=1;j<=1250;j++){
+        String csvFile = "/Users/kuangwentao/Documents/workspace/MOEAFramework/CBO/datasets/Workload/"+j+".csv";
+        String line = "";
+        String cvsSplitBy = ";";
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+        	line = br.readLine();
+        	int count = line.length() - line.replace(cvsSplitBy, "").length()+1;
+            while ((line = br.readLine()) != null&&workloadSize>0) {
+            	String[] tokens= line.split(";");            	
+                // use comma as separator             
+                
+                	workload[j-1][500-workloadSize]=tokens[3];
+                	
+                workloadSize--;
+            }
+            workloadSize=500;
+            System.out.println(Arrays.toString(workload[j-1]));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	  }
+		 String filename="/Users/kuangwentao/Documents/workspace/MOEAFramework/CBO/datasets/workload.txt";
+		 try {
+		        BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+
+		        for (int i = 0; i < workload.length; i++) {
+		        	for (int j = 0; j < workload[i].length; j++) {
+		        	    bw.write(workload[i][j] + ((j == workload[i].length-1) ? "" : ","));
+		        	}
+		            bw.newLine();
+		        }
+		        bw.flush();
+		    } catch (IOException e) {}
+		
+		
+		//System.out.println((workload[0][1]));
+    }
+}
